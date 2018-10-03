830191d727178c9ae3c9fbe9e9f79fb54eb539de
yrrslv <ypatenko@qvantel.com>
2018-10-03 17:45:26 +0300

    added Monoid

---
+++ src/main/scala/monoids/Monoid.scala
@@ -0,0 +1,43 @@
+package monoids
+
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+
+  def zero: A
+}
+
+object Monoid {
+  val stringMonoid: Monoid[String] = new Monoid[String] {
+    def op(a1: String, a2: String): String = a1 + a2
+
+    def zero = ""
+  }
+
+  def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+
+    def zero: List[A] = Nil
+  }
+
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    def op(a1: Int, a2: Int): Int = a1 + a2
+
+    def zero: Int = 0
+  }
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    def op(a1: Int, a2: Int): Int = a1 * a2
+
+    def zero: Int = 0
+  }
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
+    def zero: Boolean = false
+  }
+  val booleanAnd: Monoid[Boolean] = Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
+    def zero: Boolean = false
+  }
+
+}
