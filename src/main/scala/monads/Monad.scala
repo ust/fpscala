b328ca42f903bc98757d40468b3d7cf778736259
yrrslv <ustiner@gmail.com>
2018-11-24 18:48:57 +0200

    added basic Monads

---
+++ src/main/scala/monads/Monad.scala
@@ -0,0 +1,68 @@
+package monads
+
+import parallelism.Par
+import parallelism.Par.Par
+import propertytesting2.Gen
+import state.State
+
+import scala.language.higherKinds
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+}
+
+trait Monad[M[_]] extends Functor[M] {
+  def unit[A](a: => A): M[A]
+
+  def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
+
+  def map[A, B](ma: M[A])(f: A => B): M[B] =
+    flatMap(ma)(a => unit(f(a)))
+
+  def map2[A, B, C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
+    flatMap(ma)(a => map(mb)(b => f(a, b)))
+}
+
+object Monad {
+  val genMonad: Monad[Gen] = new Monad[Gen] {
+    def unit[A](a: => A): Gen[A] = Gen.unit(a)
+
+    def flatMap[A, B](ma: Gen[A])(f: A => Gen[B]): Gen[B] =
+      ma flatMap f
+  }
+
+  val parMonad: Monad[Par] = new Monad[Par] {
+    def unit[A](a: => A): Par[A] = Par.unit(a)
+
+    def flatMap[A, B](ma: Par[A])(f: A => Par[B]): Par[B] =
+      Par.flatMap(ma)(f)
+  }
+
+  val optionMonad: Monad[Option] = new Monad[Option] {
+    def unit[A](a: => A): Option[A] = Some(a)
+
+    def flatMap[A, B](ma: Option[A])(f: A => Option[B]): Option[B] =
+      ma flatMap f
+  }
+
+  val streamMonad: Monad[Stream] = new Monad[Stream] {
+    def unit[A](a: => A): Stream[A] = Stream(a)
+
+    def flatMap[A, B](ma: Stream[A])(f: A => Stream[B]): Stream[B] =
+      ma flatMap f
+  }
+
+  val listMonad: Monad[List] = new Monad[List] {
+    def unit[A](a: => A): List[A] = List(a)
+
+    def flatMap[A, B](ma: List[A])(f: A => List[B]): List[B] =
+      ma flatMap f
+  }
+
+  val stateMonad: Monad[State] = ???
+  //    new Monad[State] {
+  //    def unit[A](a: => A): State[A] = ???
+  //
+  //    def flatMap[A, B](ma: State[A])(f: A => State[B]): State[B] = ???
+  //  }
+}
