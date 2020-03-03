1114c10ad36a76a71c9f2babfcc7d8706d022bd9
yrrslv <ustiner@gmail.com>
2020-03-04 00:00:45 +0200

    small exercise

--- src/test/scala/JustRunner.scala
+++ src/test/scala/JustRunner.scala
@@ -1,99 +1,29 @@
-import monads._
-import propertytesting.Gen._
-import propertytesting.Prop.Result
-import propertytesting.{Gen, Prop, SGen}
-import state.RNG
-
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.language.higherKinds
 
 object JustRunner {
+  case class Thing(name: String)
+
+  implicit val ec = ExecutionContext.global
+
+  def job(args: Seq[String]): Future[Either[Throwable, Seq[Thing]]] =
+    Future.successful(Right(args.map(a => Thing("Apple" + a))))
 
   def main(args: Array[String]): Unit = {
 
+    val itemsToDo: Map[String, Seq[String]] = Map("A" -> Seq("aaa"), "B" -> Seq("bbb"))
 
-    val r0 = RNG.simple(0)
-
-    def run0(p: Prop): Result = p.run(10, 500, r0)
-
-    val genOptInt: Gen[Option[Int]] = (Gen.int ** Gen.boolean)
-      .map { case i ** b => if (b) Some(i) else None }
-    val genFnIntInt: Gen[Int => Option[Int]] =
-      Gen.int.map { i => if (i % 2 == 1) Some(_) else _ => None }
-    val genFnIntStr: Gen[Int => Option[String]] =
-      Gen.boolean.map {
-        case true => i => Some(i.toString)
-        case _ => _ => None
+    val result: Future[Either[Throwable, Map[String, Seq[Thing]]]] = Future.sequence {
+      itemsToDo.toSeq.map { case (hereTile, ecTiles) =>
+        job(ecTiles).map(_.right.map((hereTile, _)))
       }
-    val genFnStrInt: Gen[String => Option[Int]] =
-      Gen.boolean.map {
-        case true => s => Some(s.length)
-        case _ => _ => None
-      }
-    type ValidationString[+A] = Validation[String, A]
-    val valStrApplicative: Applicative[ValidationString] = Applicative.validationApplicative[String]
-    val genApplicative: Gen[Validation[String, String]] = (Gen.boolean ** Gen.character).map {
-      case (true, b) => Success("" + b + b)
-      case (_, b) => Failure("" + b + b)
-    }
+    }.map(eithers => {
+      val acc: Either[Throwable, Map[String, Seq[Thing]]] = Right(Map())
+      eithers.foldLeft(acc)((z, b) => z.right.flatMap(m => b.right.map(m + _)))
+    })
 
-    def lawAssociativeFlatMap[A, M[_]](gen: SGen[((M[A], A => M[A]), A => M[A])])
-                                      (m: Monad[M]) =
-      Prop.forAll(gen) {
-        case ((x, f), g) =>
-          m.flatMap(m.flatMap(x)(f))(g) == m.flatMap(x)(a => m.flatMap(f(a))(g))
-      }
-
-    def lawAssociativeCompose[A, B, C, D, M[_]](gen: SGen[(((A => M[B], B => M[C]), C => M[D]), A)])
-                                               (m: Monad[M]) =
-      Prop.forAll(gen) {
-        case (((f, g), h), i) =>
-          m.compose(m.compose(f, g), h)(i) == m.compose(f, m.compose(g, h))(i)
-      }
-
-    def lawIdentityCompose[A, B, C, D, M[_]](gen: SGen[(A => M[B], A)])
-                                            (m: Monad[M]) =
-      Prop.forAll(gen) { case (f, a) =>
-        val func1: A => M[B] = m.compose(m.unit[A](_), f)
-        val func2: A => M[B] = m.compose(f, (x: B) => m.unit[B](x))
-        func1(a) == f(a) && func1(a) == func2(a)
-      }
-
-    def lawIdentityFlatMap[A, B, C, D, M[_]](gen: SGen[(A => M[B], A)])
-                                            (m: Monad[M]) =
-      Prop.forAll(gen) { case (f, a) =>
-        val func1: A => M[B] = x => m.flatMap(f(x))(m.unit[B](_))
-        val func2: A => M[B] = x => m.flatMap(m.unit(x))(f)
-        func1(a) == func2(a) && func1(a) == f(a)
-      }
-
-    def lawIdentityJoin[A, B, C, D, M[_]](gen: SGen[(A => M[B], A)])
-                                         (m: Monad[M]) =
-      Prop.forAll(gen) { case (f, a) =>
-        val func1: A => M[B] = x => m.join(m.map(f(x))(m.unit[B](_)))
-        val func2: A => M[B] = x => m.join(m.map(m.unit[A](x))(f))
-        func1(a) == func2(a) && func1(a) == f(a)
-      }
-
-    def lawAssociativeJoin[A, B, C, D, M[_]](gen: SGen[(((A => M[B], B => M[C]), C => M[D]), A)])
-                                            (m: Monad[M]) =
-      Prop.forAll(gen) {
-        case (((f, g), h), i) =>
-          val func1: A => M[D] = x => m.join(m.map(m.join(m.map(f(x))(g)))(h))
-          val func2: A => M[D] = x => m.join(m.map(f(x))(y => m.join(m.map(g(y))(h))))
-          func1(i) == func2(i)
-      }
-
-    def lawIdentityMap[A, F[_]](gen: SGen[F[A]])(f: Applicative[F]) =
-      Prop.forAll(gen)(functor => f.map(functor)(x => x) == functor)
-
-
-    run0(lawAssociativeFlatMap((genOptInt ** genFnIntInt ** genFnIntInt).unsized)(Monad.optionMonad))
-    run0(lawAssociativeCompose((genFnIntStr ** genFnStrInt ** genFnIntStr ** Gen.int).unsized)(Monad.optionMonad))
-    run0(lawIdentityCompose((genFnIntStr ** Gen.int).unsized)(Monad.optionMonad))
-    run0(lawIdentityFlatMap((genFnIntStr ** Gen.int).unsized)(Monad.optionMonad))
-    run0(lawIdentityJoin((genFnIntStr ** Gen.int).unsized)(Monad.optionMonad))
-    run0(lawAssociativeJoin((genFnIntStr ** genFnStrInt ** genFnIntStr ** Gen.int).unsized)(Monad.optionMonad))
-    run0(lawIdentityMap[String, ValidationString](genApplicative.unsized)(valStrApplicative))
-
+    println(Await.result(result, 2 seconds))
   }
+
 }
