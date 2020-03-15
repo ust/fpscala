24a3e214cba4a54140ede080296d8c42d8aa01ea
yrrslv <ustiner@gmail.com>
2020-03-15 15:02:23 +0200

    Applied fix. still incorrect

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "fp scala"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies += "com.vividsolutions" % "jts" % "1.13"
--- src/main/scala/here/Triangulation.scala
+++ src/main/scala/here/Triangulation.scala
@@ -1,12 +1,15 @@
 package here
 
-import com.vividsolutions.jts.geom.{Coordinate, Geometry, GeometryFactory, Point}
+import com.vividsolutions.jts.geom._
 import com.vividsolutions.jts.triangulate.DelaunayTriangulationBuilder
 
 import collection.JavaConverters._
 
 case class Geometries(factory: GeometryFactory) {
   def point(value: Coordinate): Point = factory.createPoint(value)
+
+  def polygon(coords: Seq[Coordinate]): Polygon =
+    factory.createPolygon((coords :+ coords.head).toArray)
 }
 
 object Geometries {
@@ -30,12 +33,15 @@ object Triangulation extends App {
   }
 
   {
-    val g = triangulate(Seq(
-      coord(1, 1, 1), coord(2, 1, 1), coord(2, 2, 1)
-    ))
+//    val g = triangulate(Seq(
+//      coord(1, 1, 1), coord(2, 1, 1), coord(2, 2, 1)
+//    ))
 
-    val p = f.point(coord(1, 1, 1))
-    println( g covers p ) // fixme g is collection geometry
+
+    val t = f.polygon(Seq(coord(0, 0, 0), coord(5, 0, 0), coord(5, 5, 5), coord(0, 5, 5)))
+    val (p1, p2) = (f.point(coord(1, 1, 1)), f.point(coord(1, 1, 1.5)))
+//    println( g covers p1 ) // fixme g is collection geometry
+    println( t covers p2 )
   }
 
 
